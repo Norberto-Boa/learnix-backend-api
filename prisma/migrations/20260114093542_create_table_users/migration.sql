0ce8c0eea31feef5d14979d399ee384de287fa60
Norberto Boa <nboa.26@gmail.com>
2026-01-14 11:38:12 +0200

    feat(db): add users model

---
+++ prisma/migrations/20260114093542_create_table_users/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "ROLE" AS ENUM ('ADMIN', 'MANAGER', 'SECRETARIA');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "ROLE" NOT NULL DEFAULT 'SECRETARIA',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
