dd298a78b45572094c8834e94c1c1e2babb480c4
Norberto Boa <nboa.26@gmail.com>
2026-01-14 17:13:03 +0200

    feat(users): add find user by email on auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { PrismaModule } from '@/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from '@/env/env';
 import { EnvModule } from './env/env.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [EnvModule, PrismaModule],
+  imports: [EnvModule, PrismaModule, UsersModule],
+  providers: [UsersService],
 })
 export class AppModule {}
