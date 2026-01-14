7a369542b6bab8bb376df26626a4f2631bcb14fc
Norberto Boa <nboa.26@gmail.com>
2026-01-14 19:24:46 +0200

    feat(prisma): add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EnvModule } from './env/env.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EnvModule],
+  imports: [EnvModule, PrismaModule],
 })
 export class AppModule {}
