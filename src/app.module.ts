1c64c770de506ca0eb419926ed92e7884fe5d5f2
Norberto Boa <nboa.26@gmail.com>
2026-01-14 19:27:15 +0200

    feat(users): add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { EnvModule } from './env/env.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/user.module';
 
 @Module({
-  imports: [EnvModule, PrismaModule],
+  imports: [EnvModule, PrismaModule, UsersModule],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,16 @@
+import type { User } from '@/generated/prisma/browser';
+import { PrismaService } from '@/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findUserByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+}
