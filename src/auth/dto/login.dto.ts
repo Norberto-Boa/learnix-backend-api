0d235ce00f188e212b188e3ad421420a7c1b223e
Norberto Boa <nboa.26@gmail.com>
2026-01-14 21:07:30 +0200

    refactor(zod): remove email function deprecated

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,7 +2,7 @@ import { createZodDto } from 'nestjs-zod';
 import z from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z.email(),
   password: z.string().min(6),
 });
 
