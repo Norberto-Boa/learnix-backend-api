9ea9e68ec62b82f95f7887ac5ac7eef2626a4e6f
Norberto Boa <nboa.26@gmail.com>
2026-01-15 17:36:12 +0200

    feat(db): create student-school relationship

---
+++ prisma/migrations/20260115153351_create_user_school_relationship/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "schoolId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_schoolId_fkey" FOREIGN KEY ("schoolId") REFERENCES "schools"("id") ON DELETE SET NULL ON UPDATE CASCADE;
