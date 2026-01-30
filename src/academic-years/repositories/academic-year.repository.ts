7381969bb503f6b8f1919ca650cdb5e3e79e7385
Norberto Boa <nboa.26@gmail.com>
2026-01-30 14:28:45 +0200

    feat: add academic year repository

---
+++ src/academic-years/repositories/academic-year.repository.ts
@@ -0,0 +1,44 @@
+import { PrismaService } from '@/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@/generated/prisma/client';
+
+interface SaveAcademicYearInput {
+  year: number;
+  label: string;
+  startDate: Date;
+  endDate: Date;
+  schoolId: string;
+}
+
+@Injectable()
+export class AcademicYearsRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async findById(id: string) {
+    return this.prismaService.academicYear.findUnique({
+      where: { id },
+    });
+  }
+
+  async findByYear(year: number, schoolId: string) {
+    return this.prismaService.academicYear.findUnique({
+      where: { year_schoolId: { year, schoolId } },
+    });
+  }
+
+  async save(
+    { year, label, startDate, endDate, schoolId }: SaveAcademicYearInput,
+    tx: Prisma.TransactionClient,
+  ) {
+    const client = tx ?? this.prismaService;
+    return await client.academicYear.create({
+      data: {
+        year,
+        label,
+        startDate,
+        endDate,
+        schoolId,
+      },
+    });
+  }
+}
