c13846161514fcf30f1a3b35c493158694430d0c
Norberto Boa <nboa.26@gmail.com>
2026-02-08 22:53:26 +0200

    feat(student-documents): add repository contracts

---
+++ src/student-documents/students-documents.repository.ts
@@ -0,0 +1,27 @@
+import type { TransactionClient } from '@/generated/prisma/internal/prismaNamespace';
+import type { StudentDocumentDomain } from './domain/student-document';
+
+export interface CreateStudentsDocumentData {
+  studentId: string;
+  documentTypeId: string;
+  documentNumber: string;
+  fileUrl?: string | null;
+  schoolId: string;
+}
+
+export abstract class StudentDocumentsRepository {
+  abstract create(
+    data: CreateStudentsDocumentData,
+    tx?: TransactionClient,
+  ): Promise<StudentDocumentDomain>;
+  abstract findTypeAndNumber(
+    documentTypeId: string,
+    documentNumber: string,
+    schoolId: string,
+    tx?: TransactionClient,
+  ): Promise<StudentDocumentDomain | null>;
+  abstract findByStudent(
+    studentId: string,
+  ): Promise<StudentDocumentDomain | null>;
+  abstract delete(id: string, tx?: TransactionClient): Promise<void>;
+}
