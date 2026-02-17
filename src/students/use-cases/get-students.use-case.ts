26a3a9133c33cabf63f0c91863e26daf2a0f2807
Norberto Boa <nboa.26@gmail.com>
2026-02-17 16:08:17 +0200

    refactor(students/use-case): support filters and paginitation in getStudents

--- src/students/use-cases/get-students.use-case.ts
+++ src/students/use-cases/get-students.use-case.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { StudentsRepository } from '../repositories/students.repository';
 import { DbContext } from '../../prisma/shared/db-context';
+import { GetStudentsDTO } from '../dto/get-students.dto';
 
 @Injectable()
 export class GetStudentsUseCase {
   constructor(private studentsRepostory: StudentsRepository) {}
 
-  async execute(schoolId: string, db?: DbContext) {
-    return this.studentsRepostory.findMany(schoolId);
+  async execute(query: GetStudentsDTO, schoolId: string, db?: DbContext) {
+    const { page, limit, search, status } = query;
+
+    const result = await this.studentsRepostory.findMany(
+      {
+        schoolId,
+        page,
+        limit,
+        search,
+        status,
+      },
+      db,
+    );
+
+    return {
+      ...result,
+      page,
+      limit,
+    };
   }
 }
