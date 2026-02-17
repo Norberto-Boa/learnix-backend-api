f1b4da6543663cd1912f4dfd3459db28fe54ed7e
Norberto Boa <nboa.26@gmail.com>
2026-02-17 15:45:11 +0200

    refactor(students/repository): extend findMany to support filters and pagination

--- src/students/repositories/students.repository.ts
+++ src/students/repositories/students.repository.ts
@@ -1,7 +1,8 @@
 import type { Student } from '@/generated/prisma/client';
-import type { GENDER } from '@/generated/prisma/enums';
+import type { GENDER, STUDENT_STATUS } from '@/generated/prisma/enums';
 import type { StudentDomain } from '../domain/student';
 import type { DbContext } from '@/prisma/shared/db-context';
+import { StudentDomain } from '../domain/student';
 
 export interface CreateStudentsData {
   name: string;
@@ -11,12 +12,23 @@ export interface CreateStudentsData {
   schoolId: string;
 }
 
+export interface GetStudentsParams {
+  schoolId: string;
+  page: number;
+  limit: number;
+  search?: string;
+  status?: STUDENT_STATUS;
+}
+
 export abstract class StudentsRepository {
   abstract save(
     data: CreateStudentsData,
     db?: DbContext,
   ): Promise<StudentDomain>;
-  abstract findMany(schoolId: string, db?: DbContext): Promise<StudentDomain[]>;
+  abstract findMany(
+    params: GetStudentsParams,
+    db?: DbContext,
+  ): Promise<{ data: StudentDomain[]; total: number }>;
   abstract findById(
     id: string,
     schoolId: string,
