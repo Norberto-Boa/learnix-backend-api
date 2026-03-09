21b4b0e03a61dd87649c58f7dcbb23b2e727fb53
Norberto Boa <nboa.26@gmail.com>
2026-03-09 10:30:17 +0200

    feat(grades): add soft-delete usecase for grades

---
+++ src/grades/dto/delete-grade.use-case.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import type { GradesRepository } from '../repositories/grades.repository';
+import { GradeNotFoundError } from '../errors/grade-not-found.error';
+
+@Injectable()
+export class DeleteGradeUseCase {
+  constructor(private gradeRepository: GradesRepository) {}
+
+  async execute(id: string, schoolId: string) {
+    const grade = await this.gradeRepository.findById(id, schoolId);
+
+    if (!grade) {
+      throw new GradeNotFoundError();
+    }
+
+    await this.gradeRepository.softDelete(id, schoolId);
+  }
+}
