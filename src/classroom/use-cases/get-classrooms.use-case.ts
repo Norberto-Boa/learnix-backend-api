e7cab0901e724cfa373f169ef37c33c36c2185ee
Norberto Boa <nboa.26@gmail.com>
2026-03-11 18:09:44 +0200

    feat(classroom): implement get classrooms use case

---
+++ src/classroom/use-cases/get-classrooms.use-case.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import type { ClassroomRepository } from '../repositories/classroom.repository';
+
+interface GetClassroomUseCaseRequest {
+  gradeId?: string;
+  academicYearId?: string;
+  search?: string;
+}
+
+@Injectable()
+export class GetClassroomUseCase {
+  constructor(private classroomRepository: ClassroomRepository) {}
+
+  async execute(
+    { gradeId, academicYearId, search }: GetClassroomUseCaseRequest,
+    schoolId: string,
+  ) {
+    const classrooms = await this.classroomRepository.findMany(
+      {
+        gradeId,
+        academicYearId,
+        search,
+      },
+      schoolId,
+    );
+
+    return classrooms;
+  }
+}
