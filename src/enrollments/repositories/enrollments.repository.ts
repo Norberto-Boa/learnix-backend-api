dacafc348c9896a8e0f7d76bb7e8036d68d20b72
Norberto Boa <nboa.26@gmail.com>
2026-03-12 12:05:09 +0200

    feat(enrollments): add enrollments repository contract

---
+++ src/enrollments/repositories/enrollments.repository.ts
@@ -0,0 +1,39 @@
+import type { ENROLLMENT_STATUS } from '@/generated/prisma/enums';
+import type { DbContext } from '@/prisma/shared/db-context';
+import type { Enrollment } from '../domain/enrollment';
+
+export interface SaveEnrollmentRepositoryData {
+  studentId: string;
+  classroomId: string;
+  academicYearId: string;
+  schoolId: string;
+  status: ENROLLMENT_STATUS;
+  enrolledAt?: Date;
+}
+
+export interface GetManyEnrollmentsParams {
+  schoolId: string;
+  academicYearId?: string;
+  classroomId?: string;
+  studentId?: string;
+  status: ENROLLMENT_STATUS;
+}
+
+export abstract class EnrollmentsRepository {
+  abstract save(
+    data: SaveEnrollmentRepositoryData,
+    db?: DbContext,
+  ): Promise<Enrollment>;
+  abstract findById(id: string, schoolId: string): Promise<Enrollment | null>;
+  abstract findByStudentAndAcademicYear(
+    studentId: string,
+    academicYearId: string,
+    schoolId: string,
+  ): Promise<Enrollment | null>;
+
+  abstract countActiveEnrollmentsByClassroom(
+    classroomId: string,
+    schoolId: string,
+  ): Promise<{ count: number }>;
+  abstract findMany(params: GetManyEnrollmentsParams): Promise<Enrollment[]>;
+}
