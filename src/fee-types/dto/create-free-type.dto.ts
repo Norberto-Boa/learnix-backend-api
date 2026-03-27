4da677132a513a54f4d2ec8a4d7d968eccee7cb2
Norberto Boa <nboa.26@gmail.com>
2026-03-27 14:25:19 +0200

    feat(fee-types): add create fee type controller endpoint

--- src/fee-types/dto/create-free-type.dto.ts
+++ src/fee-types/dto/create-free-type.dto.ts
@@ -2,11 +2,11 @@ import { FEE_TYPE_CATEGORY } from "@/generated/prisma/enums";
 import { z } from "zod";
 import { createZodDto } from 'nestjs-zod';
 
-export const createFreeTypeSchema = z.object({
+export const createFeeTypeSchema = z.object({
   name: z.string().trim().min(1),
   code: z.string().trim().min(1),
   category: z.enum(FEE_TYPE_CATEGORY).optional(),
   isRecurring: z.boolean().optional()
 })
 
-export class CreateFreeTypeDTO extends createZodDto(createFreeTypeSchema) { }
+export class CreateFeeTypeDTO extends createZodDto(createFeeTypeSchema) { }
--- src/fee-types/fee-types.controller.ts
+++ src/fee-types/fee-types.controller.ts
@@ -1,6 +1,63 @@
-import { Controller } from '@nestjs/common';
+import type { AuditService } from '@/audit/audit.service';
+import type { PrismaService } from '@/prisma/prisma.service';
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import type { CreateFeeTypeUseCase } from './use-cases/create-fee-type.use-case';
+import { ZodValidationPipe } from '@/shared/pipes/zod-validation.pipe';
+import {
+  CreateFeeTypeDTO,
+  createFeeTypeSchema,
+} from './dto/create-free-type.dto';
+import { GetSchoolId } from '@/auth/decorators/get-school.decorator';
+import { GetUser } from '@/auth/decorators/get-user.decorator';
+import { RolesGuard } from '@/auth/guard/roles.guard';
+import { Roles } from '@/auth/decorators/roles.decorator';
 
+@ApiTags('Fee Types')
 @Controller('fee-types')
 export class FeeTypesController {
-  constructor() { }
+  constructor(
+    private readonly createFeeTypeUseCase: CreateFeeTypeUseCase,
+    private readonly prismaService: PrismaService,
+    private readonly auditService: AuditService,
+  ) { }
+
+  @ApiOperation({ summary: 'Creates a new fee type' })
+  @ApiResponse({ status: 201, description: 'Fee type successfully created' })
+  @ApiResponse({ status: 409, description: 'Fee type code already exists' })
+  @ApiResponse({ status: 400, description: 'Input validation error' })
+  @UseGuards(RolesGuard)
+  @Roles('MANAGER', 'ADMIN')
+  @Post()
+  async create(
+    @Body(new ZodValidationPipe(createFeeTypeSchema)) { name, code, category, isRecurring }: CreateFeeTypeDTO,
+    @GetSchoolId('schoolId') schoolId: string,
+    @GetUser('id') userId: string
+  ) {
+    return this.prismaService.$transaction(async (tx) => {
+      const feeType = await this.createFeeTypeUseCase.execute({
+        name,
+        code,
+        category,
+        isRecurring
+      }, schoolId, tx)
+
+      await this.auditService.log(
+        {
+          action: 'CREATE_FEE_TYPE',
+          entity: 'FEE_TYPE',
+          schoolId,
+          userId,
+          entityId: feeType.id,
+          newData: {
+            id: feeType.id,
+          },
+        },
+        tx
+      )
+
+      return feeType;
+    })
+
+  }
 }
