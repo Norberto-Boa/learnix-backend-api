18662d1d856b5d1fe75576a13526ab78f41ee8b2
Norberto Boa <nboa.26@gmail.com>
2026-05-18 23:56:16 +0200

    feat(enrollment-charges): add enrollment charges repository contract

---
+++ src/enrollment-charges/repositories/enrollment-charges.repository.ts
@@ -0,0 +1,81 @@
+import type { CHARGE_STATUS } from '@/generated/prisma/enums';
+import type { DbContext } from '@/prisma/shared/db-context';
+import type { EnrollmentCharge } from '../domain/enrollment-charge';
+
+export interface CreateEnrollmentChargeInput {
+  enrollmentId: string;
+  feeTypeId: string;
+  academicYearId: string;
+
+  referenceYear: number;
+  referenceMonth: number;
+
+  dueDate: Date;
+
+  baseAmount: number;
+  penaltyAmount: number;
+  totalAmount: number;
+  paidAmount: number;
+  balanceAmount: number;
+
+  status: CHARGE_STATUS;
+}
+
+export interface UpdateEnrollmentChargeInput {
+  dueDate?: Date;
+
+  baseAmount?: number;
+  penaltyAmount?: number;
+  totalAmount?: number;
+  balanceAmount?: number;
+}
+
+export interface FindManyEnrollmentChargesParams {
+  enrollmentId?: string;
+  academicYearId?: string;
+
+  referenceYear?: number;
+  referenceMonth?: number;
+
+  status?: CHARGE_STATUS;
+  page: number;
+  limit: number;
+}
+
+export abstract class EnrollmentChargesRepository {
+  abstract save(
+    data: CreateEnrollmentChargeInput,
+    schoolId: string,
+    db: DbContext,
+  ): Promise<EnrollmentCharge>;
+
+  abstract findById(
+    id: string,
+    schoolId: string,
+  ): Promise<EnrollmentCharge | null>;
+
+  abstract findDuplicatedCharge(params: {
+    enrollmentId: string;
+    feeTypeId: string;
+    referenceYear: string;
+    referenceMonth: number;
+  }): Promise<EnrollmentCharge | null>;
+
+  abstract findMany(
+    schoolId: string,
+    params: FindManyEnrollmentChargesParams,
+  ): Promise<EnrollmentCharge[]>;
+
+  abstract update(
+    data: UpdateEnrollmentChargeInput,
+    tx?: DbContext,
+  ): Promise<EnrollmentCharge[]>;
+
+  abstract cancel(
+    id: string,
+    schoolId: string,
+    tx?: DbContext,
+  ): Promise<EnrollmentCharge>;
+
+  abstract delete(id: string, schoolId: string, tx?: DbContext): Promise<void>;
+}
