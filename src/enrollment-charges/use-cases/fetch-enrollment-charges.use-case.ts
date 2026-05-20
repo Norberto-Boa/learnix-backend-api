2f7e949eeb81e30614df4f546bd8228cf590bc08
Norberto Boa <nboa.26@gmail.com>
2026-05-21 01:11:05 +0200

    feat(enrollment-charges): add fetch enrollment charges use case

---
+++ src/enrollment-charges/use-cases/fetch-enrollment-charges.use-case.ts
@@ -0,0 +1,57 @@
+import type { CHARGE_STATUS } from '@/generated/prisma/enums';
+import { EnrollmentCharge } from '../domain/enrollment-charge';
+import { Injectable } from '@nestjs/common';
+import { EnrollmentChargesRepository } from '@/enrollment-charges/repositories/enrollment-charges.repository';
+
+interface FetchEnrollmentChargesUseCaseRequest {
+  enrollmentId?: string;
+  academicYearId?: string;
+  referenceYear?: number;
+  referenceMonth?: number;
+  status?: CHARGE_STATUS;
+  page?: number;
+  limit?: number;
+}
+
+export interface FetchEnrollmentChargesUseCaseResponse {
+  enrollmentCharges: EnrollmentCharge[];
+}
+
+@Injectable()
+export class FetchEnrollmentChargesUseCase {
+  constructor(
+    private readonly enrollmentChargesRepository: EnrollmentChargesRepository,
+  ) {}
+
+  async execute(
+    {
+      academicYearId,
+      enrollmentId,
+      limit,
+      page,
+      referenceMonth,
+      referenceYear,
+      status,
+    }: FetchEnrollmentChargesUseCaseRequest,
+    schoolId: string,
+  ): Promise<FetchEnrollmentChargesUseCaseResponse> {
+    const take = limit ?? 10;
+
+    const enrollmentCharges = await this.enrollmentChargesRepository.findMany(
+      schoolId,
+      {
+        limit: take,
+        page: page ?? 1,
+        academicYearId,
+        enrollmentId,
+        referenceMonth,
+        referenceYear,
+        status,
+      },
+    );
+
+    return {
+      enrollmentCharges,
+    };
+  }
+}
